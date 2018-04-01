0337007a30091ff574779aa6f0e020e5d49770df
Aellon Krider <aellonkrider@gmail.com>
2018-04-01 16:38:37 -0400

    Update js - remove unnecessary scripts'

--- scripts.js
+++ scripts.js
@@ -16,34 +16,3 @@ $(document).ready(function(){
 });
 
 
-$(document).ready(function(){
-  $('.slider-left').slick({
-    mobileFirst: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    arrows: false,    
-    dots: false,
-    infinite: true,
-    speed: 5,
-    cssEase: 'ease',
-    appendArrows: $('.button')
-  });
-});
-
-$(document).ready(function(){
-  $('.slider-right').slick({
-    mobileFirst: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    arrows: false,    
-    dots: false,
-    infinite: true,
-    speed: 5,
-    cssEase: 'ease',
-    appendArrows: $('.button')
-  });
-});
-
-
